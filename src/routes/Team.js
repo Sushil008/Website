e769d08c070902e39e5aba5ecfe0b839e081da81
Sushil008 <sushilbaral887@gmail.com>
2023-10-04 13:13:41 +0545

    My commit

--- src/routes/Team.js
+++ src/routes/Team.js
@@ -14,18 +14,37 @@ function Team() {
       <Navbar />
       <h2>Meet our team here at Saptam Technologies</h2>
       <div className="TeamInfo">
-        <img src={CEOImg} alt="CEO" />
-        <p>Mr. Avishek Subedi, Chief Executive</p>
-        <img src={MDImg} alt="MD" />
-        <p>Mr. Sushil Baral, Managing Director</p>
-        <img src={COOImg} alt="COO" />
-        <p>Mr. Sabin Adhikari, Chief Operation Officer</p>
-        <img src={CIOImg} alt="CIO" />
-        <p>Mr. Pranay Shakya, Chief Information Officer</p>
-        <img src={PMImg} alt="PM" />
-        <p>Mr. Sujan Tiwari, Project Manager</p>
-        <img src={QAImg} alt="QA" />
-        <p>Mr. Promish Gurung, Quality Assurance</p>
+        <div className="team-member">
+          <img src={CEOImg} alt="CEO" />
+          <p>Mr. Avishek Subedi, Chief Executive</p>
+          <pre>{'"Ek game UNO khelam na ani dhama dhaam 8 bajey samma kaam garni"'}</pre>
+        </div>
+        <div className="team-member">
+          <img src={MDImg} alt="MD" />
+          <p>Mr. Sushil Baral, Managing Director</p>
+          <pre>{'"Saptam Tech have a strong reputation in the industry for its work culture, innovation, and commitment to excellence. \n We have been appreciated for the companys positive image and values."'}</pre>
+        </div>
+        <div className="team-member">
+          <img src={COOImg} alt="COO" />
+          <p>Mr. Sabin Adhikari, Chief Operation Officer</p>
+          <pre>{'"Khoi office ta cha tara k cha kasto cha kei tha nai chaina. Mah chuna ta chu tara kaha chu kina chu tha nai chaina. \n Mah office nai aunna ani kasari tha huncha sir malai ?  "'}</pre>
+        </div>
+        <div className="team-member">
+          <img src={CIOImg} alt="CIO" />
+          <p>Mr. Pranay Shakya, Chief Information Officer</p>
+          <pre>{'"Khoi KTO k bhanam ma. Jaam baru mantri uncle lai bhetna"'}</pre>
+        </div>
+        <div className="team-member">
+          <img src={PMImg} alt="PM" />
+          <p>Mr. Sujan Tiwari, Project Manager</p>
+          <pre>{'"Sujan Tiwari mero Naam AKA Raju Master. \n Malai churot khana dinchan tei vara mah yo company ma aayeko ho"'}</pre>
+          <audio src="" type="audio/mp3"></audio>
+        </div>
+        <div className="team-member">
+          <img src={QAImg} alt="QA" />
+          <p>Mr. Promish Gurung, Quality Assurance</p>
+          <pre>{'"Khoi huna ta ho, tara huna chai k ho"'}</pre>
+        </div>
       </div>  
     </>
   );
